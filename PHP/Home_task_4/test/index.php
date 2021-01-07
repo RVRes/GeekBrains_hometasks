a9b4f75720535652f6287675d3dec8a9c4ad85d0
RomanReshetnikov <rv.reshetnikov@gmail.com>
2021-01-06 20:37:07 -0600

    PHP task 4 added

--- PHP/Home_task_4/test/index.php
+++ PHP/Home_task_4/test/index.php
@@ -5,3 +5,31 @@
  * Date: 08.12.2018
  * Time: 0:22
  */
+
+//require_once ('2.php');
+//var_dump($a);
+
+//$file = fopen('log.txt', 'a');
+//fwrite($file, "string1\r\n");
+//fclose($file);
+
+//2.
+//$array = [];
+//for ($i = 1; $i <= 10; $i++) {
+//    $array [] = 'string' . $i;
+//}
+//
+//$file = fopen ('log.txt', 'a');
+//fwrite($file, implode("\r\n", $array));
+//fclose($file);
+
+//3.
+$array = [];
+for ($i = 1; $i <= 10; $i++) {
+    $array [] = 'string' . $i;
+}
+file_put_contents('log.txt',implode("\r\n", $array), FILE_APPEND);
+
+//include ('log.txt');
+
+echo file_get_contents('http://yandex.ru/');
